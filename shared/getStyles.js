1b99ff3006b2213edc9ff0a9b4d9a2cae8cf0be7
Yousef <yousef@inboxtheapp.com>
2016-10-30 01:41:15 -0400

    update

--- shared/getStyles.js
+++ shared/getStyles.js
@@ -1,11 +1,9 @@
 const conditionalStyles = require('./styles')
 
-var getStyles = function (target, styles, condition) {
+module.exports = function (target, styles, condition) {
   return Object.assign({},
     styles,
     condition && conditionalStyles[target].true,
     !condition && conditionalStyles[target].false
   )
 }
-
-module.exports = getStyles
